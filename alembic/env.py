30b0b8b9b25727907582c3297804b60f804e728a
Grunaf <roperar@tutanota.de>
2025-04-17 13:58:17 +0300

    Change source

--- alembic/env.py
+++ alembic/env.py
@@ -8,8 +8,13 @@ from sqlalchemy import engine_from_config, pool
 # Загружаем .env (если не загружается где-то выше)
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
-from config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 from app.database.models import Base
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
 # Формируем строку подключения
 SQLALCHEMY_DATABASE_URL = (
